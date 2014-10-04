1e6f85e4f13da1f22bb383dee6aa2feffeee3dd9
no-glue <nikola_pav@eunet.rs>
2014-10-04 20:46:03 +0200

    button-details component

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,12 @@
 var start = (function(can, $) {
+  can.Component.extend({
+    tag: "button-details",
+    template: "<a href='#' class='button' id='sometodo'>Details</a>",
+    scope: {
+      select: function(todo) {
+
+      }
+    }
+  });
   console.log("fucking shit");
 })(can, $);
