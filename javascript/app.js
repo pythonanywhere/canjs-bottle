f536504ff91ac98683bc277626e239164a183835
no-glue <nikola_pav@eunet.rs>
2014-10-05 14:05:05 +0200

    todo model

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,31 @@
-var start = (function(can, $, out) {
+var todos = [
+  // todos
+  {id: 1, name: "Get the thing working", detail: "Nibh invenire convenire et cum"},
+  {id: 2, name: "Get out", detail: "Nibh invenire convenire et cum"},
+  {id: 3, name: "Drink and eat", detail: "Nibh invenire convenire et cum"}
+];
+var start = (function(can, $, out, todos) {
+  var Todo = can.Model.extend({
+    // todo model
+    // fetches things and so on
+    findAll: function() {
+      // get all todos
+      return $.Deferred().resolve(todos);
+    },
+    findOne: function(params) {
+      // get one todo
+      return $.Deferred().resolve(todos[(+params.id) - 1]);
+    },
+    update: function(id, attributes) {
+      // update one todo
+      $extend(todos[id - 1], attributes);
+      return $.Deferred().resolve();
+    },
+    destroy: function() {
+      // destroy todo
+      return $.Deferred().resolve();
+    }
+  });
   can.Component.extend({
     // router component
     // handles page routing
@@ -26,4 +53,4 @@ var start = (function(can, $, out) {
   can.route(":page/:id", {page: "", id: ""});
   can.route.ready();
   console.log("fucking shit");
-})(can, $, "#out");
+})(can, $, "#out", todos);
